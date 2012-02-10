c27d3e39dc34522415b47cc4471ced789c3a3750
Sam-Mauris Yong <mauris@hotmail.sg>
2012-02-10 23:09:39 +0800

    license and sample documentation line

--- packfire/collection/sort/IComparator.php
+++ packfire/collection/sort/IComparator.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Defines an interface that allows comparing
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire/collection/sort
+ * @since 1.0
+ */
 interface IComparator {
     
     public function compare($a, $b);
