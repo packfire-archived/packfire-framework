758fcfa25015bbf76629b31276ccd9aa0d407c46
Sam-Mauris Yong <mauris@hotmail.sg>
2012-02-11 22:25:04 +0800

    action class and IRunnable
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/IRunnable.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * A class that can be executed
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire
+ * @since 1.0-sofia
+ */
+interface IRunnable {
+    
+    public function run();
+    
+}
---
+++ packfire/pAction.php
@@ -0,0 +1,46 @@
+<?php
+Packfire::load('packfire.IRunnable');
+
+/**
+ * An action class that combines the logic controller and view controller
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire
+ * @since 1.0-sofia
+ */
+class pAction implements IRunnable {
+    
+    /**
+     * The controller for this action
+     * @var pController
+     */
+    private $controller;
+    
+    /**
+     * Create a new pAction
+     * @param string $controller The name of the controller class to execute
+     * @param string $view The name of the view class to output
+     */
+    public function __construct($controller, $view){
+        Packfire::load('controller.' . $controller);
+        $this->controller = new $controller();
+        $this->controller->view($view);
+    }
+    
+    /**
+     * Get the controller
+     * @return pController Returns the controller
+     */
+    public function controller(){
+        return $this->controller;
+    }
+    
+    /**
+     * Execute the action  
+     */
+    public function run() {
+        $this->controller->run();
+    }
+    
+}
