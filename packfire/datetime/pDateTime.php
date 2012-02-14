fa1529e5f6f09f5dc42c037d64499c3514583829
Sam-Mauris Yong <mauris@hotmail.sg>
2012-02-15 01:03:27 +0800

    creation of datetime classes
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/datetime/pDateTime.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * pDateTime Description
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.datetime
+ * @since 1.0-sofia
+ */
+class pDateTime {
+    
+    //TODO: put your code here
+    
+}
---
+++ packfire/datetime/pTime.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * pTime Description
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.datetime
+ * @since 1.0-sofia
+ */
+class pTime {
+    
+    //TODO: put your code here
+    
+}
