e9ece8081a8a35a0edf8a3372612ab72bb28acd2
Sam-Mauris Yong <mauris@hotmail.sg>
2012-02-15 00:52:17 +0800

    sample application files and creation of model folder
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ public/packfire/controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Handles interaction for home
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package package
+ * @since 1.0-sofia
+ */
+class HomeController extends AppController {
+    
+    
+    
+}
