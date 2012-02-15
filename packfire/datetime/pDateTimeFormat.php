cede3d6b67a73f6d9b491584116bc9850e46e1b2
Sam-Mauris Yong <mauris@hotmail.sg>
2012-02-15 16:39:12 +0800

    date time format standards class
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/datetime/pDateTimeFormat.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Date Time standard formats
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.datetime
+ * @since 1.0-sofia
+ */
+class pDateTimeFormat {
+    
+    const ATOM = 'Y-m-d\TH:i:sP';
+    const COOKIE = 'l, d-M-y H:i:s T' ;
+    const ISO8601 = 'Y-m-d\TH:i:sO' ;
+    const RFC822 = 'D, d M y H:i:s O' ;
+    const RFC850 = 'l, d-M-y H:i:s T' ;
+    const RFC1036 = 'D, d M y H:i:s O' ;
+    const RFC1123 = 'D, d M Y H:i:s O' ;
+    const RFC2822 = 'D, d M Y H:i:s O' ;
+    const RFC3339 = 'Y-m-d\TH:i:sP' ;
+    const RSS = 'D, d M Y H:i:s O' ;
+    const W3C = 'Y-m-d\TH:i:sP';
+    
+}
