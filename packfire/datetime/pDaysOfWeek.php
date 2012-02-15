b5b95d7437c703732d912f3d20e5410c8e3ae2af
Sam-Mauris Yong <mauris@hotmail.sg>
2012-02-15 16:18:21 +0800

    datetime constants
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/datetime/pDaysOfWeek.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Constants for Days of the Week
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.datetime
+ * @since 1.0-sofia
+ */
+class pDaysOfWeek {
+    
+    /**
+     * Saturday
+     */
+    const SATURDAY = 0;
+    
+    /**
+     * Sunday
+     */
+    const SUNDAY = 1;
+    
+    /**
+     * Monday
+     */
+    const MONDAY = 2;
+    
+    /**
+     * Tuesday
+     */
+    const TUESDAY = 3;
+    
+    /**
+     * Wednesday
+     */
+    const WEDNESDAY = 4;
+    
+    /**
+     * Thursday
+     */
+    const THURSDAY = 5;
+    
+    /**
+     * Friday
+     */
+    const FRIDAY = 6;
+    
+}
