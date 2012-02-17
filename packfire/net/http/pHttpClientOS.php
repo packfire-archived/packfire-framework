9b36fc0028c2567eb78d3c656c3ee4855f3f288d
Sam-Mauris Yong <mauris@hotmail.sg>
2012-02-17 15:42:22 +0800

    http client browser and OS constants
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/net/http/pHttpClientOS.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Constants for Client Operating Systems
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.net.http
+ * @since 1.0-sifa
+ */
+class pHttpClientOS {
+
+    /**
+     * Operating System: Microsoft Windows
+     */
+    const WINDOWS = 'Windows';
+
+    /**
+     * Operating System: Apple Macintosh
+     */
+    const MACINTOSH = 'Macintosh';
+
+    /**
+     * Operating System: Linux
+     */
+    const LINUX = 'Linux';
+
+    /**
+     * Operating System: UNIX
+     */
+    const UNIX = 'UNIX';
+
+    /**
+     * Operating System: Unknown
+     */
+    const UNKNOWN = '';
+    
+    /**
+     * Apple's iOS 
+     */
+    const IOS = 'iOS';
+    
+    /**
+     * BlackBerry  
+     */
+    const BLACKBERRY = 'Blackberry';
+    
+    /**
+     * Google Android OS 
+     */
+    const ANDROID = 'Android';
+    
+}
