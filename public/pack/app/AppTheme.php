cab9b8755d5dc82d583547526d85dd9271aeb525
Sam-Mauris Yong <mauris@hotmail.sg>
2012-02-18 00:33:13 +0800

    added AppTheme abstraction
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ public/pack/app/AppTheme.php
@@ -0,0 +1,14 @@
+<?php
+pload('packfire.view.pTheme');
+
+/**
+ * Application Theme abstraction class
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.app
+ * @since 1.0-sofia
+ */
+abstract class AppTheme extends pTheme {
+    
+}
