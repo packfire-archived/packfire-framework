c6d3d9eed5c28566e0e7138897b357b2f0bc8f76
Sam-Mauris Yong <mauris@hotmail.sg>
2012-02-20 00:11:14 +0800

    Service loader abstraction
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/ioc/IServiceLoader.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ServiceLoader abstraction
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.ioc
+ * @since 1.0-sofia
+ */
+interface IServiceLoader {
+    
+    public function load();
+    
+}
