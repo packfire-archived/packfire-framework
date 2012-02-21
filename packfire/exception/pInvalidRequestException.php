22aa461713796a1170c98ee38caf27d8f1e26292
Sam-Mauris Yong <mauris@hotmail.sg>
2012-02-21 12:16:36 +0800

    creation of mid-level exception classes
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/exception/pInvalidRequestException.php
@@ -0,0 +1,15 @@
+<?php
+pload('pException');
+
+/**
+ * pInvalidRequestException Description
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.exception
+ * @since 1.0-sofia
+ */
+class pInvalidRequestException extends pException {
+    
+    
+}
---
+++ packfire/exception/pLogicException.php
@@ -0,0 +1,16 @@
+<?php
+pload('pException');
+
+/**
+ * A logical exception
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.exception
+ * @since 1.0-sofia
+ */
+class pLogicException extends pException {
+    
+    
+    
+}
