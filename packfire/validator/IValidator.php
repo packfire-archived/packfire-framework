20ba0c84befa1ef7f2bf531e65e6d81ce69bc6eb
Sam-Mauris Yong <mauris@hotmail.sg>
2012-03-14 15:25:10 +0800

    created validator interface
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/validator/IValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Validator abstraction
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.validator
+ * @since 1.0-sofia
+ */
+interface IValidator {
+    
+    public function validate($value);
+    
+}
