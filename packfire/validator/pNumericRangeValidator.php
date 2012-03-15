fc94c41ee1e53264ba0726d798e29d1c34f928c2
Sam-Mauris Yong <mauris@hotmail.sg>
2012-03-15 11:38:19 +0800

    implementation of data type and numeric range validators
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/validator/pNumericRangeValidator.php
@@ -0,0 +1,50 @@
+<?php
+pload('IValidator');
+
+/**
+ * Numerical range validator
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.validator
+ * @since 1.0-sofia
+ */
+class pNumericRangeValidator implements IValidator {
+    
+    /**
+     * Minimum 
+     * @var integer|double 
+     * @since 1.0-sofia
+     */
+    private $min;
+    
+    /**
+     * Maximum
+     * @var integer|double 
+     * @since 1.0-sofia
+     */
+    private $max;
+    
+    /**
+     * Create a new numerical range validator pNumericRangeValidator
+     * @param integer|double $min The minimum value
+     * @param integer|double $max The maximum value
+     * @since 1.0-sofia
+     */
+    public function __construct($min, $max){
+        $this->min = $min;
+        $this->max = $max;
+    }
+    
+    /**
+     * Validate the value
+     * @param integer|double $value
+     * @return integer|double Returns true if the validation succeeded,
+     *                        false otherwise.
+     * @since 1.0-sofia
+     */
+    public function validate($value) {
+        return $this->min < $value && $this->max > $value;
+    }
+    
+}
