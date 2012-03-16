74cd5347fdc82b90856f2939c7e252f1f0a0ba50
Sam-Mauris Yong <mauris@hotmail.sg>
2012-03-16 11:29:20 +0800

    numeric validator implementation
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/validator/pNumericValidator.php
@@ -0,0 +1,25 @@
+<?php
+pload('IValidator');
+
+/**
+ * Numeric value validator
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.validator
+ * @since 1.0-sofia
+ */
+class pNumericValidator implements IValidator {
+    
+    /**
+     * Validate the value
+     * @param mixed $value The value to validate
+     * @return boolean Returns true if the validation succeeded,
+     *                        false otherwise.
+     * @since 1.0-sofia
+     */
+    public function validate($value){
+        return is_numeric($value);
+    }
+    
+}
