55fbeb88c0f984882393aa8c86e5e06f57c4c0d9
Sam-Mauris Yong <mauris@hotmail.sg>
2012-03-22 16:00:51 +0800

    more documentation updates
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- packfire/database/drivers/mysql/linq/pMySqlLinqJoin.php
+++ packfire/database/drivers/mysql/linq/pMySqlLinqJoin.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * pMySqlLinqJoin Description
+ * A MySQL LINQ Join statement
  *
  * @author Sam-Mauris Yong / mauris@hotmail.sg
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
@@ -10,12 +10,50 @@
  */
 class pMySqlLinqJoin {
     
+    /**
+     * The target table
+     * @var string
+     * @since 1.0-sofia
+     */
     private $target;
+    
+    /**
+     * The outer table to join
+     * @var string
+     * @since 1.0-sofia
+     */
     private $source;
+    
+    /**
+     * The key in the inner table
+     * @var string
+     * @since 1.0-sofia
+     */
     private $innerKey;
+    
+    /**
+     * The key in the outer table
+     * @var string
+     * @since 1.0-sofia
+     */
     private $outerKey;
+    
+    /**
+     * The selector
+     * @var string
+     * @since 1.0-sofia
+     */
     private $selector;
     
+    /**
+     * Create a new pMySqlLinqJoin object
+     * @param string $target The table targeted
+     * @param string $source The table to join
+     * @param string $innerKey The inner key
+     * @param string $outerKey The outer key
+     * @param string $selector The selector 
+     * @since 1.0-sofia
+     */
     public function __construct($target, $source, $innerKey, $outerKey, $selector){
         $this->source = $source;
         $this->target = $target;
@@ -24,6 +62,11 @@ class pMySqlLinqJoin {
         $this->selector = $selector;
     }
     
+    /**
+     * Create the statement
+     * @return string 
+     * @since 1.0-sofia
+     */
     public function create(){
         $join = '';
         if($this->selector){
--- packfire/database/drivers/mysql/linq/pMySqlLinqOrder.php
+++ packfire/database/drivers/mysql/linq/pMySqlLinqOrder.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * pMySqlLinqOrder Description
+ * A MySQL LINQ Order statement
  *
  * @author Sam-Mauris Yong / mauris@hotmail.sg
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
@@ -10,15 +10,37 @@
  */
 class pMySqlLinqOrder {
     
+    /**
+     * The field to sort
+     * @var string
+     * @since 1.0-sofia
+     */
     private $field;
-    
+ 
+    /**
+     * Flags whether the order is descending or not
+     * @var boolean
+     * @since 1.0-sofia
+     */
     private $descending;
     
+    /**
+     * Create a new pMySqlLinqOrder object
+     * @param string $field The field to sort
+     * @param boolean $descending (optional) Sets whether the order is descending
+     *                or not, defaults to false.
+     * @since 1.0-sofia
+     */
     public function __construct($field, $descending = false){
         $this->descending = $descending;
         $this->field = $field;
     }
     
+    /**
+     * Create the statement
+     * @return string
+     * @since 1.0-sofia
+     */
     public function create(){
         return $this->field . ($this->descending ? ' DESC': '');;
     }
