3b4f96c82529c0536ad770730e6e23819dd44167
Sam-Mauris Yong <mauris@hotmail.sg>
2012-03-27 10:05:29 +0800

    documentation for pDebugger

--- packfire/debugger/pDebugger.php
+++ packfire/debugger/pDebugger.php
@@ -2,7 +2,7 @@
 pload('pConsoleDebugOutput');
 
 /**
- * pDebugger Description
+ * The debugger to help you debug in your application
  *
  * @author Sam-Mauris Yong / mauris@hotmail.sg
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
@@ -12,19 +12,26 @@ pload('pConsoleDebugOutput');
 class pDebugger extends pBucketUser {
     
     /**
-     *
+     * The output channel to write logs to
      * @var IDebugOutput
+     * @since 1.0-sofia
      */
     private $output;
     
     /**
-     *
-     * @param IDebugOutput $output 
+     * Create a new pDebugger object
+     * @param IDebugOutput $output The output to write the debugging logs to
+     * @since 1.0-sofia
      */
     public function __construct($output){
         $this->output = $output;
     }
-    
+
+    /**
+     * Write a variable dump to the debugger log
+     * @param mixed $value The variable to dump
+     * @since 1.0-sofia 
+     */
     public function dump($value){
         $output = var_export($value, true);
         $dbt = reset(debug_backtrace());
@@ -32,6 +39,11 @@ class pDebugger extends pBucketUser {
         $this->output->write($output, $where, __METHOD__);
     }
     
+    /**
+     * Write a log message
+     * @param string $message The log message to be written
+     * @since 1.0-sofia 
+     */
     public function log($message){
         $dbt = reset(debug_backtrace());
         $where = sprintf('%s:%d', pPath::baseName($dbt['file']), $dbt['line']);
@@ -39,8 +51,9 @@ class pDebugger extends pBucketUser {
     }
     
     /**
-     *
-     * @param pException $exception 
+     * Log an exception's details
+     * @param Exception $exception The exception to be logged
+     * @since 1.0-sofia
      */
     public function exception($exception){
         $where = sprintf('%s:%d', pPath::baseName($exception->getFile()), $exception->getLine());
@@ -48,12 +61,22 @@ class pDebugger extends pBucketUser {
         $this->output->write($message, $where, __METHOD__);
     }
     
+    /**
+     * Do a time check log.
+     * Time taken from the application load to reach the time check will be shown on the log
+     * @since 1.0-sofia 
+     */
     public function timeCheck(){
         $dbt = reset(debug_backtrace());
         $message = sprintf('Time taken from application loaded to reach %s line %s', $dbt['file'], $dbt['line']);
         $this->output->write($message, $this->service('timer.app.start')->result(), __METHOD__);
     }
     
+    /**
+     * Perform output when the debugger is destroyed, supposedly application end execution.
+     * @internal
+     * @ignore 
+     */
     public function __destruct(){
         $this->output->output();
     }
