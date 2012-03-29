18f0bcfcaa91bd178e8ccf4db073d9a68dffd395
Sam-Mauris Yong <mauris@hotmail.sg>
2012-03-29 08:43:32 +0800

    documentation for pload() and helper.php

--- packfire/helper.php
+++ packfire/helper.php
@@ -1,10 +1,24 @@
 <?php
 require(__PACKFIRE_ROOT__ . 'pClassLoader.php');
 
-function pload($path){
+/**
+ * The helper file where alias and functions are declared
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire
+ * @since 1.0-sofia
+ */
+
+/**
+ * The shortened alias of the class loader. 
+ * @param string $package The package to load.
+ * @since 1.0-sofia 
+ */
+function pload($package){
     static $loader = null;
     if(!$loader){
         $loader = new pClassLoader();
     }
-    $loader->load($path);
+    $loader->load($package);
 }
