44f71e4f3b728aec0850927edb1721257bca9c5c
Sam-Mauris Yong <mauris@hotmail.sg>
2012-04-07 18:18:22 +0800

    documentation updates for pValidationFilter and copyright info added for packfire.controller package
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- packfire/controller/pValidationFilter.php
+++ packfire/controller/pValidationFilter.php
@@ -2,6 +2,15 @@
 pload('IControllerFilter');
 pload('packfire.exception.pValidationException');
 
+/**
+ * A filter for validators
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.controller
+ * @since 1.0-sofia
+ */
 class pValidationFilter implements IControllerFilter {
     
     /**
@@ -11,10 +20,22 @@ class pValidationFilter implements IControllerFilter {
      */
     private $validator;
     
+    /**
+     * Create a new pValidationFilter object
+     * @param IValidator $validator The validator
+     * @since 1.0-sofia
+     */
     public function __construct($validator){
         $this->validator = $validator;
     }
     
+    /**
+     * Performs the filter operation
+     * @param mixed $value The value to validate
+     * @return mixed Returns the original value
+     * @throws pValidationException Thrown when validation fails.
+     * @since 1.0-sofia
+     */
     public function filter($value) {
         if(!$this->validator->validate($value)){
             throw new pValidationException('Validation failed on value.', 1020);
