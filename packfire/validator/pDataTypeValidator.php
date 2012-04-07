66f1e4cb9e747df928031812e414a4e7c38c725e
Sam-Mauris Yong <mauris@hotmail.sg>
2012-04-07 19:13:03 +0800

    copyright info added for packfire.validator package
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- packfire/validator/pDataTypeValidator.php
+++ packfire/validator/pDataTypeValidator.php
@@ -5,6 +5,7 @@ pload('IValidator');
  * Data type validator
  *
  * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
  * @package packfire.validator
  * @since 1.0-sofia
--- packfire/validator/pStringLengthRangeValidator.php
+++ packfire/validator/pStringLengthRangeValidator.php
@@ -6,6 +6,7 @@ pload('pNumericRangeValidator');
  * String length range validator
  *
  * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
  * @package packfire.validator
  * @since 1.0-sofia
