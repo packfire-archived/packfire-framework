4896cd76b2b3976cdb1342a9f9237a0dc961d1b4
Sam-Mauris Yong <mauris@hotmail.sg>
2012-04-07 19:09:55 +0800

    copyright info added for packfire.template package
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- packfire/template/pHtmlTemplate.php
+++ packfire/template/pHtmlTemplate.php
@@ -5,6 +5,7 @@ pload('pTemplate');
  * A HTML template
  *
  * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
  * @package packfire.template
  * @since 1.0-sofia
