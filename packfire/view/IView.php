682e0b32c29112bb1e3cd34628336e5447f5a7fa
Sam-Mauris Yong <mauris@hotmail.sg>
2012-04-07 19:14:15 +0800

    copyright info added and documentation updates for packfire.view package
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- packfire/view/IView.php
+++ packfire/view/IView.php
@@ -4,6 +4,7 @@
  * View interface that provides an output
  *
  * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
  * @package packfire.view
  * @since 1.0-sofia
--- packfire/view/pView.php
+++ packfire/view/pView.php
@@ -7,6 +7,7 @@ pload('packfire.template.pTemplate');
  * The generic view class.
  *
  * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
  * @package packfire.view
  * @since 1.0-sofia
