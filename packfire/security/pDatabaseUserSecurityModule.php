454078dd5069773af4967c38d0f7ee97af58f5a1
Sam-Mauris Yong <mauris@hotmail.sg>
2012-04-07 17:08:14 +0800

    pDatabaseUserSecurityModule documentation and code fixes
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- packfire/security/pDatabaseUserSecurityModule.php
+++ packfire/security/pDatabaseUserSecurityModule.php
@@ -1,15 +1,21 @@
 <?php
+pload('ISecurityModule');
 
 /**
- * pDatabaseUserSecurityModule Description
+ * A default database-based security module implementation
  *
  * @author Sam-Mauris Yong / mauris@hotmail.sg
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
  * @package packfire.security
- * @since version-created
+ * @since 1.0-sofia
  */
 class pDatabaseUserSecurityModule implements ISecurityModule {
     
+    /**
+     * The user identity
+     * @var mixed
+     * @since 1.0-sofia
+     */
     private $user;
     
     public function authenticate($token) {
@@ -29,8 +35,11 @@ class pDatabaseUserSecurityModule implements ISecurityModule {
         $user = null;
         if(func_num_args() == 1){
             if($newIdentity){
-                $user = $this->service('database')->from('users')->get(array('Username' => $newIdentity));
-                $this->service('session')->set('security.user.id', $user['UserId']);
+                $user = $this->service('database')
+                        ->from('users')
+                        ->get(array('Username' => $newIdentity));
+                $this->service('session')
+                        ->set('security.user.id', $user['UserId']);
             }else{
                 $this->session('session')->removeAt('security.user.id');
             }
@@ -39,7 +48,8 @@ class pDatabaseUserSecurityModule implements ISecurityModule {
         }else{
             $id = $this->service('session')->get('security.user.id');
             if($id !== null){
-                $user = $this->service('database')->from('users')->get(array('UserId' => $id));
+                $user = $this->service('database')
+                        ->from('users')->get(array('UserId' => $id));
             }
         }
         if($user){
