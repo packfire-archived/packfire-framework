1b3f4e4d3e6fe3ae1933932a42ebdba198242b60
Sam-Mauris Yong <mauris@hotmail.sg>
2012-04-08 22:45:41 +0800

    package naming update for IExceptionHandler and pExceptionHandler
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- packfire/exception/handler/pExceptionHandler.php
+++ packfire/exception/handler/pExceptionHandler.php
@@ -8,7 +8,7 @@ pload('pExceptionPageView');
  * @author Sam-Mauris Yong / mauris@hotmail.sg
  * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
- * @package packfire.exception
+ * @package packfire.exception.handler
  * @since 1.0-sofia
  */
 class pExceptionHandler implements IExceptionHandler {
