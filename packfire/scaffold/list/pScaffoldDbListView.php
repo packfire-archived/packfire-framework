63cb428e3a314c2e6cf664dbd89086d2e771067a
Sam-Mauris Yong <mauris@hotmail.sg>
2012-04-09 16:39:59 +0800

    pScaffoldDbListView package update

--- packfire/scaffold/list/pScaffoldDbListView.php
+++ packfire/scaffold/list/pScaffoldDbListView.php
@@ -10,7 +10,7 @@ pload('packfire.net.http.pUrl');
  * @author Sam-Mauris Yong / mauris@hotmail.sg
  * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
- * @package packfire.scaffold
+ * @package packfire.scaffold.list
  * @since 1.0-sofia
  */
 class pScaffoldDbListView extends pView {
