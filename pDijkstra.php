4806d2583692e144d647f1b047397e334ba10f5d
Sam-Mauris Yong <mauris@hotmail.sg>
2012-04-19 17:22:50 +0800

    graph, vertex, path and Dijkstra's Algorithm implementation
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ pDijkstra.php
@@ -0,0 +1,158 @@
+<?php
+pload('packfire.collection.pList');
+pload('pGraphPath');
+
+/**
+ * Resolve and find a path between the starting and ending vertices in a graph.
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.graph
+ * @since 1.0-sofia
+ */
+class pDijkstra {
+    
+    /**
+     * The graph to work and solve on
+     * @var pGraph
+     * @since 1.0-sofia
+     */
+    private $graph;
+    
+    /**
+     * The starting vertex
+     * @var IVertex 
+     * @since 1.0-sofia
+     */
+    private $startVertex;
+    
+    /**
+     * The ending vertex
+     * @var IVertex
+     * @since 1.0-sofia
+     */
+    private $endVertex;
+    
+    /**
+     * The list of paths found
+     * @var pList 
+     * @since 1.0-sofia
+     */
+    private $paths = null;
+    
+    /**
+     * Create a new Dijkstra object
+     * @param pGraph $graph The graph to work and solve
+     * @since 1.0-sofia
+     */
+    public function __construct($graph){
+        $this->graph = $graph;
+        foreach($this->graph->vertices() as $vertex){
+            /* @var $vertex IVertex */
+            $vertex->reset();
+        }
+    }
+    
+    /**
+     * Set the starting vertex
+     * @param IVertex $start The vertex to start working the shortest path from
+     * @return pDijkstra Returns itself for chaining purposes.
+     * @since 1.0-sofia
+     */
+    public function start($start){
+        $this->startVertex = $start;
+        $this->paths = new pList();
+        $this->paths->add(new pList(array($start)));
+        return $this;
+    }
+    
+    /**
+     * Set the end vertex
+     * @param IVertex $end The vertex to end at.
+     * @return pDijkstra Returns itself for chaining purposes.
+     * @since 1.0-sofia
+     */
+    public function end($end){
+        $this->endVertex = $end;
+        return $this;
+    }
+    
+    /**
+     * Find the shortest path between the start and end vertex
+     * @param IVertex $start (optional) Set the starting vertex to start finding
+     *                      the path.
+     * @param IVertex $end (optional) Set the ending vertex
+     * @return pGraphPath Returns the resolved path if found, or null
+     *              if no path is found.
+     * @since 1.0-sofia
+     */
+    public function find($start = null, $end = null){
+        if(func_num_args() > 0){
+            $this->start($start);
+        }
+        if(func_num_args() == 2){
+            $this->end($end);
+        }
+        
+        $this->calculatePotentials($this->startVertex, $this->endVertex);
+        
+        $aPath = array();
+        $vertex = $this->endVertex;
+        
+        // loop through where the pathfinding came from
+        while ($vertex->id() != $this->startVertex->id()) {
+            $aPath[] = $vertex;
+            $vertex = $vertex->from();
+            if($vertex == null){
+                return null;
+            }
+        }
+        
+        $aPath[] = $this->startVertex;
+        $path = new pGraphPath(new pList(array_reverse($aPath)));
+        return $path;
+    }
+    
+    /**
+     * Calculate the potentials to every other vertex from the start vertex
+     * @param IVertex $vertex The vertex to calculate potentials
+     * @param IVertex $end The destination vertex
+     * @since 1.0-sofia
+     */
+    private function calculatePotentials($vertex, $end){
+        $connections = $vertex->connections();
+        $sorted = array_flip($connections->toArray());
+        krsort($sorted);
+        foreach($connections as $id => $cost){
+            /* @var $connection IVertex */
+            $connection = $this->graph->get($id);
+            if($vertex->id() == $connection->id()){
+                echo 'dead';
+                break;
+            }
+            $connection->setPotential($vertex->potential() + $cost, $vertex);
+            
+            foreach($this->paths as $path){
+                /* @var $path pList */
+                $last = $path->last();
+                if($last && $last->id() == $vertex->id()){
+                    $this->paths->add(new pList(
+                            array_merge($path->toArray(), array($connection))
+                        ));
+                }
+            }
+        }
+        
+        // mark the vector as you have already passed by
+        $vertex->mark();
+        
+        foreach($sorted as $id){
+            $nextVertex = $this->graph->get($id);
+            if(!$nextVertex->passed()){
+                $this->calculatePotentials($nextVertex, $end);
+            }
+        }
+    }
+    
+}
---
+++ pGraph.php
@@ -0,0 +1,72 @@
+<?php
+pload('pDijkstra');
+
+/**
+ * A graph representation
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.graph
+ * @since 1.0-sofia
+ */
+class pGraph {
+    
+    /**
+     * The vertices in the graph
+     * @var pMap 
+     * @since 1.0-sofia
+     */
+    private $vertices;
+    
+    /**
+     * Create a new Graph object 
+     * @since 1.0-sofia
+     */
+    public function __construct(){
+        $this->vertices = new pMap();
+    }
+    
+    /**
+     * Add a vertex to the graph
+     * @param IVertex $vertex The vertex to be added
+     * @since 1.0-sofia
+     */
+    public function add($vertex){
+        $this->vertices[$vertex->id()] = $vertex;
+    }
+    
+    /**
+     * Get a vertex by its ID
+     * @param string|integer $vertexId The identifier of the vertex
+     * @return IVertex The vertex found is returned.  
+     * @since 1.0-sofia
+     */
+    public function get($vertexId){
+        return $this->vertices->get($vertexId);
+    }
+    
+    /**
+     * Get all the vertices in the graph
+     * @return pMap Returns all the vertices in the graph
+     * @since 1.0-sofia
+     */
+    public function vertices(){
+        return $this->vertices;
+    }
+    
+    /**
+     * Find the shortest path between two vertices in the graph
+     * @param string|integer $start The identifier of the start vertex
+     * @param string|integer $end The identifier of the end vertex
+     * @return pGraphPath Returns the path if found or null if no path is found.
+     * @since 1.0-sofia
+     */
+    public function find($start, $end){
+        $dijkstra = new Dijkstra($this);
+        $path = $dijkstra->start($this->get($start))
+                ->end($this->get($end))->find();
+        return $path;
+    }
+    
+}
