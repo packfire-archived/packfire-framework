c5f3c7e8ae6a49d6ec9ebdc8ca84a5b3c3762193
Sam-Mauris Yong <mauris@hotmail.sg>
2012-04-25 15:53:46 +0800

    documentation updates and new constants for pNewline
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- packfire/text/pNewline.php
+++ packfire/text/pNewline.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Newline constants
+ * Newline constants and functionality to work with newlines
  *
  * @author Sam-Mauris Yong / mauris@hotmail.sg
  * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
@@ -30,10 +30,25 @@ class pNewline {
     const UNIX = "\n";
     
     /**
+     * The HTML &lt;br&gt;
+     * @since 1.0-sofia 
+     */
+    const HTML_BR = '<br>';
+    
+    /**
+     * The HTML &lt;br /&gt;
+     * @since 1.0-sofia 
+     */
+    const XHTML_BR = '<br />';
+    
+    /**
      * Neutralize all newline character format in a string
      * @param string $string The string to be neutralized
-     * @param string $target (optional) The target newline to use. Defaults to pNewline::UNIX.
-     * @return string Get the neutralized string
+     * @param string $target (optional) The target newline to use. 
+     *              Defaults to pNewline::UNIX.
+     *              You can use constants from pNewline or any text to
+     *              replace the newline.
+     * @return string Returns the neutralized string
      * @since 1.0-sofia
      */
     public static function neutralize($string, $target = self::UNIX){
