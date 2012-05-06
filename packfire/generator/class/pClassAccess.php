b15d7bd5dc7de5f8fcd1da0da504328775778d98
Sam-Mauris Yong <mauris@hotmail.sg>
2012-05-06 21:41:16 +0800

    implementation of new classes for the Generator package
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

---
+++ packfire/generator/class/pClassAccess.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Class method access types
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.generator.class
+ * @since 1.0-sofia
+ */
+class pClassAccess {
+    
+    /**
+     * Public access
+     * @since 1.0-sofia 
+     */
+    const ACCESS_PUBLIC = 1;
+    
+    /**
+     * Private access
+     * @since 1.0-sofia 
+     */
+    const ACCESS_PRIVATE = 2;
+    
+    /**
+     * Protected access
+     * @since 1.0-sofia 
+     */
+    const ACCESS_PROTECTED = 3;
+    
+}
