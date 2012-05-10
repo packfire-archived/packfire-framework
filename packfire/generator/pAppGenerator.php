8c376d8abd2f1d0669e8186a843950c184b33c2e
Sam-Mauris Yong <mauris@hotmail.sg>
2012-05-10 15:58:01 +0800

    updated pAppGenerator to take in the packfire root folder and bug fixes
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- packfire/generator/pAppGenerator.php
+++ packfire/generator/pAppGenerator.php
@@ -21,13 +21,24 @@ class pAppGenerator {
     private $root;
     
     /**
+     * The root folder that contains an installation of
+     *          Packfire Framework
+     * @var string
+     * @since 1.0-sofia
+     */
+    private $packfire;
+    
+    /**
      * Create a new pAppGenerator
      * @param string $root The root folder to install a new copy of the 
      *          application
+     * @param string $packfire The root folder that contains an installation of
+     *          Packfire Framework
      * @since 1.0-sofia
      */
-    public function __construct($root){
+    public function __construct($root, $packfire){
         $this->root = $root;
+        $this->packfire = $packfire;
     }
     
     /**
@@ -60,101 +71,103 @@ class pAppGenerator {
      * @since 1.0-sofia 
      */
     public function generate(){
-        $this->renderFile('resource/application/packfire-favicon.ico',
+        $path = dirname(__FILE__);
+        $this->renderFile($path . '/resource/application/packfire-favicon.ico',
                 pPath::combine($this->root, 'packfire-favicon.ico'));
-        $this->renderFile('resource/application/.htaccess',
+        $this->renderFile($path . '/resource/application/.htaccess',
                 pPath::combine($this->root, '.htaccess'));
-        $this->renderFile('resource/application/index.php',
-                pPath::combine($this->root, 'index.php'));
+        $this->renderFile($path . '/resource/application/index.php',
+                pPath::combine($this->root, 'index.php'),
+                array('packfirePath' => $this->packfire));
         
-        $this->renderFile('resource/application/assets/images/readme.md',
+        $this->renderFile($path . '/resource/application/assets/images/readme.md',
                 pPath::combine($this->root, 'assets/images/readme.md'));
-        $this->renderFile('resource/application/assets/images/packfire.biglogo.dark.png',
+        $this->renderFile($path . '/resource/application/assets/images/packfire.biglogo.dark.png',
                 pPath::combine($this->root, 'assets/images/packfire.biglogo.dark.png'));
-        $this->renderFile('resource/application/assets/images/packfire.biglogo.light.png',
+        $this->renderFile($path . '/resource/application/assets/images/packfire.biglogo.light.png',
                 pPath::combine($this->root, 'assets/images/packfire.biglogo.light.png'));
         
-        $this->renderFile('resource/application/assets/scripts/readme.md',
+        $this->renderFile($path . '/resource/application/assets/scripts/readme.md',
                 pPath::combine($this->root, 'assets/scripts/readme.md'));
-        $this->renderFile('resource/application/assets/scripts/packfire.js',
+        $this->renderFile($path . '/resource/application/assets/scripts/packfire.js',
                 pPath::combine($this->root, 'assets/scripts/packfire.js'));
-        $this->renderFile('resource/application/assets/scripts/pfdebugger.js',
+        $this->renderFile($path . '/resource/application/assets/scripts/pfdebugger.js',
                 pPath::combine($this->root, 'assets/scripts/pfdebugger.js'));
         
-        $this->renderFile('resource/application/assets/styles/readme.md',
+        $this->renderFile($path . '/resource/application/assets/styles/readme.md',
                 pPath::combine($this->root, 'assets/styles/readme.md'));
-        $this->renderFile('resource/application/assets/styles/dark.js',
+        $this->renderFile($path . '/resource/application/assets/styles/dark.css',
                 pPath::combine($this->root, 'assets/styles/dark.js'));
-        $this->renderFile('resource/application/assets/styles/global.js',
+        $this->renderFile($path . '/resource/application/assets/styles/global.css',
                 pPath::combine($this->root, 'assets/styles/global.js'));
-        $this->renderFile('resource/application/assets/styles/light.js',
+        $this->renderFile($path . '/resource/application/assets/styles/light.css',
                 pPath::combine($this->root, 'assets/styles/light.js'));
         
-        $this->renderFile('resource/application/pack/app/AppController.php',
+        $this->renderFile($path . '/resource/application/pack/app/AppController.php',
                 pPath::combine($this->root, 'pack/app/AppController.php'));
-        $this->renderFile('resource/application/pack/app/AppModel.php',
+        $this->renderFile($path . '/resource/application/pack/app/AppModel.php',
                 pPath::combine($this->root, 'pack/app/AppModel.php'));
-        $this->renderFile('resource/application/pack/app/AppSecurityModule.php',
+        $this->renderFile($path . '/resource/application/pack/app/AppSecurityModule.php',
                 pPath::combine($this->root, 'pack/app/AppSecurityModule.php'));
-        $this->renderFile('resource/application/pack/app/AppTemplate.php',
+        $this->renderFile($path . '/resource/application/pack/app/AppTemplate.php',
                 pPath::combine($this->root, 'pack/app/AppTemplate.php'));
-        $this->renderFile('resource/application/pack/app/AppTheme.php',
+        $this->renderFile($path . '/resource/application/pack/app/AppTheme.php',
                 pPath::combine($this->root, 'pack/app/AppTheme.php'));
-        $this->renderFile('resource/application/pack/app/AppView.php',
+        $this->renderFile($path . '/resource/application/pack/app/AppView.php',
                 pPath::combine($this->root, 'pack/app/AppView.php'));
-        $this->renderFile('resource/application/pack/app/Application.php',
+        $this->renderFile($path . '/resource/application/pack/app/Application.php',
                 pPath::combine($this->root, 'pack/app/Application.php'));
-        $this->renderFile('resource/application/pack/app/readme.md',
+        $this->renderFile($path . '/resource/application/pack/app/readme.md',
                 pPath::combine($this->root, 'pack/app/readme.md'));
         
-        $this->renderFile('resource/application/pack/readme.md',
+        $this->renderFile($path . '/resource/application/pack/readme.md',
                 pPath::combine($this->root, 'pack/readme.md'));
         
-        $this->renderFile('resource/application/pack/config/app.local.yml',
+        $this->renderFile($path . '/resource/application/pack/config/app.local.yml',
                 pPath::combine($this->root, 'pack/config/app.local.yml'));
-        $this->renderFile('resource/application/pack/config/app.yml',
+        $this->renderFile($path . '/resource/application/pack/config/app.yml',
                 pPath::combine($this->root, 'pack/config/app.yml'));
-        $this->renderFile('resource/application/pack/config/ioc.yml',
+        $this->renderFile($path . '/resource/application/pack/config/ioc.yml',
                 pPath::combine($this->root, 'pack/config/ioc.yml'));
-        $this->renderFile('resource/application/pack/config/routing.yml',
+        $this->renderFile($path . '/resource/application/pack/config/routing.yml',
                 pPath::combine($this->root, 'pack/config/routing.yml'));
-        $this->renderFile('resource/application/pack/config/readme.md',
+        $this->renderFile($path . '/resource/application/pack/config/readme.md',
                 pPath::combine($this->root, 'pack/config/readme.md'));
         
-        $this->renderFile('resource/application/pack/controller/HomeController.php',
+        $this->renderFile($path . '/resource/application/pack/controller/HomeController.php',
                 pPath::combine($this->root, 'pack/controller/HomeController.php'));
-        $this->renderFile('resource/application/pack/controller/ThemeSwitchController.php',
+        $this->renderFile($path . '/resource/application/pack/controller/ThemeSwitchController.php',
                 pPath::combine($this->root, 'pack/controller/ThemeSwitchController.php'));
-        $this->renderFile('resource/application/pack/controller/readme.md',
+        $this->renderFile($path . '/resource/application/pack/controller/readme.md',
                 pPath::combine($this->root, 'pack/controller/readme.md'));
         
-        $this->renderFile('resource/application/pack/library/readme.md',
+        $this->renderFile($path . '/resource/application/pack/library/readme.md',
                 pPath::combine($this->root, 'pack/library/readme.md'));
-        $this->renderFile('resource/application/pack/model/readme.md',
+        $this->renderFile($path . '/resource/application/pack/model/readme.md',
                 pPath::combine($this->root, 'pack/model/readme.md'));
         
-        $this->renderFile('resource/application/pack/storage/cache/readme.md',
+        $this->renderFile($path . '/resource/application/pack/storage/cache/readme.md',
                 pPath::combine($this->root, 'pack/storage/cache/readme.md'));
-        $this->renderFile('resource/application/pack/storage/log/readme.md',
+        $this->renderFile($path . '/resource/application/pack/storage/log/readme.md',
                 pPath::combine($this->root, 'pack/storage/log/readme.md'));
-        $this->renderFile('resource/application/pack/storage/temp/readme.md',
+        $this->renderFile($path . '/resource/application/pack/storage/temp/readme.md',
                 pPath::combine($this->root, 'pack/storage/temp/readme.md'));
         
-        $this->renderFile('resource/application/pack/template/homeIndex.html',
+        $this->renderFile($path . '/resource/application/pack/template/homeIndex.html',
                 pPath::combine($this->root, 'pack/template/homeIndex.html'));
-        $this->renderFile('resource/application/pack/template/readme.md',
+        $this->renderFile($path . '/resource/application/pack/template/readme.md',
                 pPath::combine($this->root, 'pack/template/readme.md'));
         
-        $this->renderFile('resource/application/pack/theme/readme.md',
+        $this->renderFile($path . '/resource/application/pack/theme/readme.md',
                 pPath::combine($this->root, 'pack/theme/readme.md'));
-        $this->renderFile('resource/application/pack/theme/DarkTheme.php',
+        $this->renderFile($path . '/resource/application/pack/theme/DarkTheme.php',
                 pPath::combine($this->root, 'pack/theme/DarkTheme.php'));
-        $this->renderFile('resource/application/pack/theme/LightTheme.php',
+        $this->renderFile($path . '/resource/application/pack/theme/LightTheme.php',
                 pPath::combine($this->root, 'pack/theme/LightTheme.php'));
         
-        $this->renderFile('resource/application/pack/view/home/HomeIndex.php',
-                pPath::combine($this->root, 'pack/view/home/HomeIndex.php'));
-        $this->renderFile('resource/application/pack/view/readme.md',
+        $this->renderFile($path . '/resource/application/pack/view/home/HomeIndexView.php',
+                pPath::combine($this->root, 'pack/view/home/HomeIndexView.php'));
+        $this->renderFile($path . '/resource/application/pack/view/readme.md',
                 pPath::combine($this->root, 'pack/view/readme.md'));
     }
     
