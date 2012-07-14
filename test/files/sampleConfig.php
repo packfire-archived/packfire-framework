fd218fd5d6c4e8218de271462079bc71ed9d67cd
Sam-Mauris Yong <mauris@hotmail.sg>
2012-07-14 10:34:01 +0800

    PHP configuration test implementation

---
+++ test/files/sampleConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+    'first_section' => array(
+        'one' => 1,
+        'five' => 5,
+        'animal' => 'BIRD'
+    ),
+    'second_section' => array(
+        'path' => '/usr/local/bin',
+        'URL' => 'http://www.example.com/~username'
+    ),
+    'third_section' => array(
+        'phpversion' => array(
+            '5.0',
+            '5.1',
+            '5.2',
+            '5.3'
+        )
+    )
+);
