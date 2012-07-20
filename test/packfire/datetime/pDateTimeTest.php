1879a5b3561f9f9bd88c5a8cf37b93c7fce695bf
Sam-Mauris Yong <mauris@hotmail.sg>
2012-07-20 15:03:49 +0800

    updated previous commit's test

--- test/packfire/datetime/pDateTimeTest.php
+++ test/packfire/datetime/pDateTimeTest.php
@@ -9,6 +9,11 @@ pload('packfire.datetime.pDaysOfWeek');
  */
 class pDateTimeTest extends PHPUnit_Framework_TestCase {
 
+    
+    public function __construct(){
+        date_default_timezone_set('UTC');
+    }
+    
     /**
      * @var pDateTime
      */
@@ -20,7 +25,6 @@ class pDateTimeTest extends PHPUnit_Framework_TestCase {
      */
     protected function setUp() {
         $this->object = new pDateTime(1999, 6, 24, 10, 30, 35);
-        date_default_timezone_set('UTC');
     }
 
     /**
@@ -28,7 +32,7 @@ class pDateTimeTest extends PHPUnit_Framework_TestCase {
      * This method is called after a test is executed.
      */
     protected function tearDown() {
-        date_default_timezone_set(ini_get('date.timezone'));
+        
     }
 
     /**
