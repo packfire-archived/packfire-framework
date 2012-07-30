b363fd00ae439f4eea152957897e6f5d48f3a6df
Sam-Mauris Yong <mauris@hotmail.sg>
2012-07-30 10:18:57 +0800

    CLI routing classes initial implementation
    [ci skip]

---
+++ packfire/routing/cli/pCliRoute.php
@@ -0,0 +1,27 @@
+<?php
+pload('packfire.routing.IRoute');
+
+/**
+ * pCliRoute class
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2012, Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.routing.cli
+ * @since 1.0-elenor
+ */
+class pCliRoute implements IRoute {
+    
+    public function __construct($name, $data) {
+        
+    }
+
+    public function match($request) {
+        
+    }
+
+    public function name() {
+        
+    }
+    
+}
---
+++ packfire/routing/cli/pCliRouter.php
@@ -0,0 +1,27 @@
+<?php
+pload('packfire.routing.pRouter');
+
+/**
+ * pCliRouter class
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2012, Sam-Mauris Yong / mauris@hotmail.sg
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packafire.routing.cli
+ * @since 1.0-elenor
+ */
+class pCliRouter extends pRouter {
+    
+    public function load() {
+        
+    }
+
+    protected function prepareRoute($route, $params) {
+        
+    }
+
+    protected function routeFactory($key, $data) {
+        
+    }
+    
+}
