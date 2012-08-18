5b0425af226c17c8b3d0adf143cad9926f8f5285
Sam-Mauris Yong <mauris@hotmail.sg>
2012-08-18 13:58:37 +0800

    OAuth client class implementation

---
+++ packfire/oauth/pOAuthClient.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * pOAuthClient Description
+ *
+ * @author Sam Yong
+ * @copyright Copyright (c) 2012, Sam Yong
+ * @license Expression license is undefined on line 8, column 15 in Templates/Scripting/Packfire Framework/PackfireClass.php.
+ * @package package
+ * @since version-created
+ */
+class pOAuthClient {
+    
+    //TODO: put your code here
+    
+}
