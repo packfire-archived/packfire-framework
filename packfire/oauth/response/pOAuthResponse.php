5cf47c24a280ccdd2719a2ef5e9a3723a36d7c6d
Sam-Mauris Yong <mauris@hotmail.sg>
2012-08-18 13:58:20 +0800

    namespace updates

--- packfire/oauth/response/pOAuthResponse.php
+++ packfire/oauth/response/pOAuthResponse.php
@@ -9,7 +9,7 @@ pload('packfire.application.http.pHttpAppResponse');
  * @author Sam-Mauris Yong / mauris@hotmail.sg
  * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
- * @package packfire.oauth.http
+ * @package packfire.oauth.response
  * @since 1.1-sofia
  */
 abstract class pOAuthResponse extends pHttpAppResponse {
