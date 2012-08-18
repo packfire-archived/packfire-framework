d34508f613981516abc0778bb8e02aff90afb6b9
Sam-Mauris Yong <mauris@hotmail.sg>
2012-08-18 13:58:08 +0800

    access and request token response class implementation

---
+++ packfire/oauth/response/pOAuthRequestTokenResponse.php
@@ -0,0 +1,19 @@
+<?php
+pload('pOAuthResponse');
+
+/**
+ * pOAuthRequestTokenResponse class
+ * 
+ * OAuth Request Token Response
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package packfire.oauth.response
+ * @since 1.1-sofia
+ */
+class pOAuthRequestTokenResponse extends pOAuthResponse {
+    
+    
+    
+}
