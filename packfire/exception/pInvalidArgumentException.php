ace554859c30c94249dd039daa9f9fc6578b7af1
Sam-Mauris Yong <mauris@hotmail.sg>
2012-09-27 12:16:33 +0800

    pInvalidArgumentException $given is now optional

--- packfire/exception/pInvalidArgumentException.php
+++ packfire/exception/pInvalidArgumentException.php
@@ -19,12 +19,12 @@ class pInvalidArgumentException extends pException {
      * @param string $method The class method that is raising this exception
      * @param string $argument The argument that has an invalid value
      * @param string $expectation The expecting value
-     * @param string $given The actual value that was given
+     * @param string $given (optional) The actual value that was given
      * @param string $code (optional) The error code if any
      * @since 1.0-sofia
      */
-    public function __construct($method, $argument, $expectation, $given, $code = null) {
-        $message = sprintf('%s() expects $%s to be %s. %s given instead.',
+    public function __construct($method, $argument, $expectation, $given = null, $code = null) {
+        $message = sprintf('%s() expects $%s to be %s.' . (func_num_args() > 3 ? ' %s was given instead.' : ''),
                 $method, $argument, $expectation, (string)$given);
         parent::__construct($message, $code);
     }
