b02c646d9ae19d911ed24663931b3142a47cc2d5
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-12 13:38:50 +0800

    test bootstrapper independence

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,22 @@
 <?php
 
 define('__PACKFIRE_START__', microtime(true));
-include('src/Packfire/Bootstrap.php');
-Packfire\Bootstrap::initialize();
+
+define('__APP_ROOT__', '');
+
+set_include_path(realpath(__DIR__ . '/../src')
+        . PATH_SEPARATOR . get_include_path());
+spl_autoload_register(function($class) {
+    $class = ltrim($class, '\\');
+    $fileName  = '';
+    $namespace = '';
+    $lastNsPos = strrpos($class, '\\');
+    if($lastNsPos){
+        $namespace = substr($class, 0, $lastNsPos);
+        $class = substr($class, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace)
+                . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
+    require_once($fileName);
+});
