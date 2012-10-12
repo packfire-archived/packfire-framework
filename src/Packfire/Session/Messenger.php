8ab146ef903ada146049f960971059aafef1815b
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-12 14:55:31 +0800

    Session package tests updated

--- src/Packfire/Session/Messenger.php
+++ src/Packfire/Session/Messenger.php
@@ -5,7 +5,7 @@ use Packfire\IoC\BucketUser;
 
 /**
  * Messenger class
- * 
+ *
  * Cross-class and controller session messenger. You can utilize this service
  * to provide messenging between classes and controllers.
  *
@@ -16,25 +16,25 @@ use Packfire\IoC\BucketUser;
  * @since 1.0-sofia
  */
 class Messenger extends BucketUser {
-    
+
     /**
      * Create a new Messenger object
-     * @since 1.0-sofia 
+     * @since 1.0-sofia
      */
     public function __construct(){
-        
+
     }
-    
+
     /**
      * Get the session bucket instance
      * @return SessionBucket Returns the session bucket instance
      * @since 1.0-sofia
      */
     private function session(){
-        $bucket = $this->service('session')->bucket('PackfireMessenger');
+        $bucket = $this->service('session')->bucket('Messenger');
         return $bucket;
     }
-    
+
     /**
      * Build the session key for the name and recepient
      * @param string $name Name of the message
@@ -48,7 +48,7 @@ class Messenger extends BucketUser {
         }
         return '$'. $recepient . '/' . $name;
     }
-    
+
     /**
      * Send a message to the recepient(s)
      * @param string $name Name of the message
@@ -67,14 +67,14 @@ class Messenger extends BucketUser {
             $this->session()->set($this->buildKey($name, $recepient), $message);
         }
     }
-    
+
     /**
      * Check if there is a message
      * @param string $name The name of the message to check
      * @param string $recepient (optional) The recepient of the message. If not set,
      *                      the message will be checked in the global scope.
      * @return boolean Returns true if the message exists, false otherwise.
-     * @since 1.0-sofia 
+     * @since 1.0-sofia
      */
     public function check($name, $recepient = null){
         if(func_num_args() == 1){
@@ -85,7 +85,7 @@ class Messenger extends BucketUser {
         }
         return $this->session()->has($this->buildKey($name, $recepient));
     }
-    
+
     /**
      * Read the message content. Message will be removed after read.
      * @param string $name The name of the message to check
@@ -107,13 +107,13 @@ class Messenger extends BucketUser {
         $this->session()->remove($key);
         return $content;
     }
-    
+
     /**
      * Clear all messenges
-     * @since 1.0-sofia 
+     * @since 1.0-sofia
      */
     public function clear(){
         $this->session()->clear();
     }
-    
+
 }
