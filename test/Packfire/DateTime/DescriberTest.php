14de8bb16384ca480719e928967fafc6a7517126
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-15 16:21:23 +0800

    Describer test updated

--- test/Packfire/DateTime/DescriberTest.php
+++ test/Packfire/DateTime/DescriberTest.php
@@ -6,7 +6,7 @@ class DescriberTest extends \PHPUnit_Framework_TestCase {
     private $object;
     
     protected function setUp(){
-        $object = new Describer();
+        $this->object = new Describer();
     }
     
     protected function tearDown(){
@@ -16,6 +16,6 @@ class DescriberTest extends \PHPUnit_Framework_TestCase {
     public function testDescribe(){
         $dt1 = new DateTime(2012, 10, 15, 12, 30, 00);
         $dt2 = new DateTime(2012, 10, 15, 12, 35, 00);
-        $this->assertEquals('5 min', $this->object->describe($dt1, $dt2));
+        $this->assertEquals('5 mins ago', $this->object->describe($dt1, $dt2));
     }
 }
