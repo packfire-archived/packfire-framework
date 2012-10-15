5a16a9f2fa51f8754aeb04a18c7e9e9587f00f35
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-16 00:37:40 +0800

    Describer::limit() test implementation

--- test/Packfire/DateTime/DescriberTest.php
+++ test/Packfire/DateTime/DescriberTest.php
@@ -25,6 +25,24 @@ class DescriberTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals('4 days, 1 hour and 5 mins', $this->object->describe($dt1, $dt2));
     }
     
+    public function testLimit(){
+        $this->assertNull($this->object->limit());
+        $dt1 = new DateTime(2012, 10, 11, 11, 30, 00);
+        $dt2 = new DateTime(2012, 10, 15, 12, 35, 00);
+        $this->object->limit(1);
+        $this->assertEquals(1, $this->object->limit());
+        $this->assertEquals('4 days', $this->object->describe($dt1, $dt2));
+    }
+    
+    public function testLimit2(){
+        $this->assertNull($this->object->limit());
+        $dt1 = new DateTime(2012, 10, 11, 11, 30, 00);
+        $dt2 = new DateTime(2012, 10, 15, 12, 35, 00);
+        $this->object->limit(2);
+        $this->assertEquals(2, $this->object->limit());
+        $this->assertEquals('4 days and 1 hour', $this->object->describe($dt1, $dt2));
+    }
+    
     public function testListing(){
         $this->assertTrue($this->object->listing());
         $this->object->listing(false);
