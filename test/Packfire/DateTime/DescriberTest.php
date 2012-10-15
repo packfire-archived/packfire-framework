e1b15d9558000e188159cc67cc1feb84fbebbe5a
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-15 12:41:17 +0800

    Packfire\DateTime\Describer test implementation

---
+++ test/Packfire/DateTime/DescriberTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Packfire\DateTime;
+
+class DescriberTest extends \PHPUnit_Framework_TestCase {
+    
+    private $object;
+    
+    protected function setUp(){
+        $object = new Describer();
+    }
+    
+    protected function tearDown(){
+        
+    }
+    
+    public function testDescribe(){
+        $dt1 = new DateTime(2012, 10, 15, 12, 30, 00);
+        $dt2 = new DateTime(2012, 10, 15, 12, 35, 00);
+        $this->assertEquals('5 min', $this->object->describe($dt1, $dt2));
+    }
+}
