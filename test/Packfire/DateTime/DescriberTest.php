d2be51b9e9729c2797d82c08c920993c59abed83
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-16 00:57:30 +0800

    test Describer::quantify implementation

--- test/Packfire/DateTime/DescriberTest.php
+++ test/Packfire/DateTime/DescriberTest.php
@@ -25,6 +25,14 @@ class DescriberTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals('4 days, 1 hour and 5 mins', $this->object->describe($dt1, $dt2));
     }
     
+    public function testQuantify(){
+        $this->assertTrue($this->object->quantify());
+        $this->object->quantify(false);
+        $dt1 = new DateTime(2012, 10, 11, 9, 30, 00);
+        $dt2 = new DateTime(2012, 10, 15, 12, 35, 00);
+        $this->assertEquals('4 day, 3 hour and 5 min', $this->object->describe($dt1, $dt2));
+    }
+    
     public function testLimit(){
         $this->assertNull($this->object->limit());
         $dt1 = new DateTime(2012, 10, 11, 11, 30, 00);
