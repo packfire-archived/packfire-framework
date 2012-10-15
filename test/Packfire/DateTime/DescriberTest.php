097033c83fccf389750a388e7e14597979a7617e
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-16 00:31:28 +0800

    Describer::listing() test updated

--- test/Packfire/DateTime/DescriberTest.php
+++ test/Packfire/DateTime/DescriberTest.php
@@ -16,6 +16,19 @@ class DescriberTest extends \PHPUnit_Framework_TestCase {
     public function testDescribe(){
         $dt1 = new DateTime(2012, 10, 15, 12, 30, 00);
         $dt2 = new DateTime(2012, 10, 15, 12, 35, 00);
-        $this->assertEquals('5 mins ago', $this->object->describe($dt1, $dt2));
+        $this->assertEquals('5 mins', $this->object->describe($dt1, $dt2));
+    }
+    
+    public function testDescribe2(){
+        $dt1 = new DateTime(2012, 10, 11, 11, 30, 00);
+        $dt2 = new DateTime(2012, 10, 15, 12, 35, 00);
+        $this->assertEquals('4 days, 1 hour and 5 mins', $this->object->describe($dt1, $dt2));
+    }
+    
+    public function testListing(){
+        $this->object->listing(false);
+        $dt1 = new DateTime(2012, 10, 11, 11, 30, 00);
+        $dt2 = new DateTime(2012, 10, 15, 12, 35, 00);
+        $this->assertEquals('4 days 1 hour 5 mins', $this->object->describe($dt1, $dt2));
     }
 }
