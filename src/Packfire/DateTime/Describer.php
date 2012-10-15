04f4e336d9d642adc557c27e1e0bfa09f927d21e
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-16 00:31:44 +0800

    listing setting implementation for Describer

--- src/Packfire/DateTime/Describer.php
+++ src/Packfire/DateTime/Describer.php
@@ -6,14 +6,46 @@ use Packfire\Text\Text;
 
 class Describer {
     
+    /**
+     * The components of the 
+     * @var array
+     * @since 2.0.0
+     */
+    private $components = array('day', 'hour', 'minute', 'second');
+    
+    private $verbs = array('day' => 'day', 'hour' => 'hour',
+        'minute' => 'min', 'second' => 'sec');
+    
+    private $listing = true;
+    
     public function __construct(){
         
     }
     
     /**
+     * Get or set if Describer should perform text listing on description
+     * @param boolean $listing (optional) If set to true, listing will be done
+     *              on the description, false otherwise.
+     * @return boolean Returns whether listing is flagged.
+     * @since 2.0.0
+     */
+    public function listing($listing = null){
+        if(func_num_args()){
+            $this->listing = $listing;
+        }
+        return $this->listing;
+    }
+    
+    /**
+     * Describe the time period between two date/time
      * 
-     * @param DateTime $dateTime1
-     * @param DateTime $dateTime2 (optional)
+     * Produces results such as : 1 hour, 24 mins and 20 secs
+     * 
+     * @param DateTime $dateTime1 The first date time
+     * @param DateTime $dateTime2 (optional) The second date time. If not set,
+     *              the current date/time will be used.
+     * @return string Returns the text description of the time period
+     * @since 2.0.0
      */
     public function describe($dateTime1, $dateTime2 = null){
         if(!$dateTime2){
@@ -25,14 +57,16 @@ class Describer {
         }else{
             $desc = array();
             /* @var $timeSpan TimeSpan */
-            $verbs = array('day' => 'day', 'hour' => 'hr', 'minute' => 'min', 'second' => 'sec');
-            foreach($verbs as $full => $short){
-                $value = $timeSpan->$full();
-                if($value > 0){
-                    $desc[] = $value . ' ' . Inflector::quantify($value, $short);
+            foreach($this->components as $component){
+                if(isset($this->verbs[$component])){
+                    $value = $timeSpan->$component();
+                    if($value > 0){
+                        $desc[] = $value . ' ' 
+                                . Inflector::quantify($value, $this->verbs[$component]);
+                    }
                 }
             }
-            return Text::listing($desc) . ' ago';
+            return $this->listing ? Text::listing($desc) : implode(' ', $desc);
         }
     }
     
