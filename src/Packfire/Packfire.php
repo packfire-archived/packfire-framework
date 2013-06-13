d7f67151b1061b36d223cd3d25e96f30fb50ab61
Sam-Mauris Yong <sam@mauris.sg>
2013-06-13 23:09:43 +0800

    updated packfire _APP_ROOT_ constant

--- src/Packfire/Packfire.php
+++ src/Packfire/Packfire.php
@@ -48,7 +48,7 @@ class Packfire
     public function __construct()
     {
         if (!defined('__APP_ROOT__')) {
-            define('__APP_ROOT__', dirname($_SERVER['SCRIPT_FILENAME']) . DIRECTORY_SEPARATOR . 'pack');
+            define('__APP_ROOT__', dirname($_SERVER['SCRIPT_FILENAME']) . '/../app');
         }
         if (!class_exists('Packfire\Core\ClassLoader\PackfireClassFinder')) {
             if (is_dir(__DIR__ . '/../../vendor')) {
