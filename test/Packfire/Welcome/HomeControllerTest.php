3831bbb7667f9922696c8dc441779e94d2edd63d
Sam-Mauris Yong <mauris@hotmail.sg>
2013-12-21 11:42:46 +0800

    updated test coverage home controller

--- test/Packfire/Welcome/HomeControllerTest.php
+++ test/Packfire/Welcome/HomeControllerTest.php
@@ -54,17 +54,6 @@ class HomeControllerTest extends \PHPUnit_Framework_TestCase
         call_user_func($this->object, $this->ioc);
     }
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-    }
-
-    /**
-     * @covers \Packfire\Welcome\HomeController::getIndex
-     */
     public function testGetIndex()
     {
         $this->object->getIndex();
@@ -72,9 +61,6 @@ class HomeControllerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('<!DOCTYPE html>', substr($this->ioc['response']->body(), 0, 15));
     }
 
-    /**
-     * @covers \Packfire\Welcome\HomeController::cliIndex
-     */
     public function testCliIndex()
     {
         ob_start();
