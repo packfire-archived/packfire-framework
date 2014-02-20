5d6aa3a6ce56c4f7ea6306155831e7b2d2aa3fa1
Sam-Mauris Yong <mauris@hotmail.sg>
2014-02-20 22:18:37 +0800

    added loader interface and class

---
+++ src/Packfire/Framework/Package/Loader.php
@@ -0,0 +1,11 @@
+<?php /*
+ * Copyright (C) 2014 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the Packfire Framework project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
+ */
+
+namespace Packfire\Framework\Package;
+
+class Loader implements LoaderInterface
+{
+}
