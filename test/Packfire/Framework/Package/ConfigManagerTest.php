c0dfcd4d8ebe855fc04821cc9680be27ea0e7c95
Sam-Mauris Yong <mauris@hotmail.sg>
2014-02-26 08:24:39 +0800

    implemented some test for configmanager

---
+++ test/Packfire/Framework/Package/ConfigManagerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Packfire\Framework\Package;
+
+use Packfire\Config\Config;
+use PHPUnit_Framework_TestCase;
+
+class ConfigManagerTest extends PHPUnit_Framework_TestCase
+{
+    public function testCommitGet()
+    {
+        $cfg = new ConfigManager();
+
+        $config1 = new Config();
+        $config1->set('sec1', 'key1', '10');
+
+        $cfg->commit('first', $config1);
+        $this->assertEquals($config1, $cfg->get('first'));
+        $this->assertEquals('10', $cfg->get('first')->get('sec1', 'key1'));
+
+        $config2 = new Config();
+        $config2->set('sec2', 'key1', '3');
+
+        $cfg->commit('first', $config2);
+        $this->assertEquals('3', $cfg->get('first')->get('sec2', 'key1'));
+        $this->assertEquals('10', $cfg->get('first')->get('sec1', 'key1'));
+    }
+
+    /**
+     * @expectedException Packfire\Framework\Exceptions\ConfigNotFoundException
+     */
+    public function testGetNotFound()
+    {
+        $cfg = new ConfigManager();
+        $cfg->get('died');
+    }
+}
