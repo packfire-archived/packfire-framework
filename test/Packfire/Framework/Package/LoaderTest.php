362b0d173e06bc7c77487da37992934884cfc719
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-13 09:32:57 +0800

    added loader test for testing load() method

--- test/Packfire/Framework/Package/LoaderTest.php
+++ test/Packfire/Framework/Package/LoaderTest.php
@@ -14,4 +14,16 @@ class LoaderTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($cfg, $loader->config());
     }
+
+    public function testLoad()
+    {
+        $cfg = new ConfigManager();
+
+        $loader = new Loader($cfg);
+        $loader->load(__DIR__ . '/../../../samplePackage');
+
+        $this->assertEquals($cfg, $loader->config());
+        $this->assertTrue(isset($cfg['test']));
+        $this->assertEquals('localhost', $cfg['test']->get('database', 'default', 'host'));
+    }
 }
